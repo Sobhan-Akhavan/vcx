96de2ef0f39ece9c83c2fc36dba5b4d6d1aa50cd
Sobhan Akhavan <sobhan.khvn@gmail.com>
2024-01-16 18:31:48 +0330

    add persian language

--- src/main/java/ir/vcx/data/entity/GenreType.java
+++ src/main/java/ir/vcx/data/entity/GenreType.java
@@ -1,18 +1,23 @@
 package ir.vcx.data.entity;
 
+import lombok.AllArgsConstructor;
+
 /**
  * Created by Sobhan on 11/9/2023 - VCX
  */
 
+@AllArgsConstructor
 public enum GenreType {
-    ACTION,
-    ADVENTURE,
-    ANIMATION,
-    COMEDY,
-    DEVOTIONAL,
-    DRAMA,
-    HISTORICAL,
-    HORROR,
-    SCIENCE_FICTION,
-    WESTERN
+    ACTION("اکشن"),
+    ADVENTURE("ماجراجویانه"),
+    ANIMATION("انیمیشن"),
+    COMEDY("کمدی"),
+    DEVOTIONAL("فداکارانه"),
+    DRAMA("درام"),
+    HISTORICAL("تاریخی"),
+    HORROR("ترسناک"),
+    SCIENCE_FICTION("علمی تخیلی"),
+    WESTERN("وسترن");
+
+    private final String value;
 }
